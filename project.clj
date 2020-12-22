2b8f7669686b144fe00df15227e518b23d85526d
Adrian Smith <smith.adriane@gmail.com>
2020-12-22 13:34:33 -0800

    update to latest membrane version.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
-                 [com.phronemophobic/membrane "0.9.14-beta"]
+                 [com.phronemophobic/membrane "0.9.16-beta"]
                  [re-frame "1.0.0-rc3"]
                  [com.googlecode.lanterna/lanterna "3.0.2"]]
   :aliases
